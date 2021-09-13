a05ba617d0e9ab8f5aedec956723fd9f37928db0
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-13 20:30:12 +0600

    cleanded document structure memory leak and code

--- HackerRank/Structuring the Document.c
+++ HackerRank/Structuring the Document.c
@@ -1,7 +1,8 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
 
@@ -11,17 +12,17 @@ struct word {
 
 struct sentence {
     struct word *data;
-    int word_count; // denotes number of words in a sentence
+    int word_count;//denotes number of words in a sentence
 };
 
 struct paragraph {
     struct sentence *data;
-    int sentence_count; // denotes number of sentences in a paragraph
+    int sentence_count;//denotes number of sentences in a paragraph
 };
 
 struct document {
     struct paragraph *data;
-    int paragraph_count; // denotes number of paragraphs in a document
+    int paragraph_count;//denotes number of paragraphs in a document
 };
 typedef struct word Word;
 typedef struct sentence Sentence;
@@ -29,68 +30,63 @@ typedef struct paragraph Paragraph;
 typedef struct document Document;
 
 void word_constructor(Word *w, int *w_len) {
-    w->data = (char *)(malloc(MAX_CHARACTERS * sizeof(char)));
+    w->data = (char *) (malloc(MAX_CHARACTERS * sizeof(char)));
     memset(w->data, 0, MAX_CHARACTERS * sizeof(char));
     *w_len = 0;
 }
 
 void sentence_constructor(Sentence *s) {
-    s->data       = (Word *)malloc(MAX_CHARACTERS * sizeof(Word));
+    s->data = (Word *) malloc(MAX_CHARACTERS * sizeof(Word));
     s->word_count = 0;
 }
 
 void paragraph_constructor(Paragraph *p) {
-    p->data           = (Sentence *)malloc(MAX_CHARACTERS * sizeof(Sentence));
+    p->data = (Sentence *) malloc(MAX_CHARACTERS * sizeof(Sentence));
     p->sentence_count = 0;
+}
+
+void document_constructor(Document *d) {
+    d->data = (Paragraph *) malloc(MAX_PARAGRAPHS * sizeof(Paragraph));
+    d->paragraph_count = 0;
 }
 
 struct document get_document(char *text) {
     Word w;
     int w_len;
     word_constructor(&w, &w_len);
-
     Sentence s;
     sentence_constructor(&s);
-
     Paragraph p;
     paragraph_constructor(&p);
-
     Document doc;
-    doc.data            = (Paragraph *)malloc(MAX_PARAGRAPHS * sizeof(Paragraph));
-    doc.paragraph_count = 0;
+    document_constructor(&doc);
 
-    for (int i = 0; text[i] != 0; ++i) {
-        if (text[i] == ' ') {
-            if (w_len > 0)
+    for (int i = 0;; ++i) {
+        int skip_this_char = 0;
+        if (text[i] == 0 || text[i] == ' ' || text[i] == '.' || text[i] == '\n') {
+            if (w_len > 0) {
                 s.data[s.word_count++] = w;
-            word_constructor(&w, &w_len);
-        } else if (text[i] == '.') {
-            if (w_len > 0)
-                s.data[s.word_count++] = w;
-            word_constructor(&w, &w_len);
-            if (s.word_count > 0)
-                p.data[p.sentence_count++] = s;
-            sentence_constructor(&s);
-        } else if (text[i] == '\n') {
-            if (w_len > 0)
-                s.data[s.word_count++] = w;
-            word_constructor(&w, &w_len);
-            if (s.word_count > 0)
-                p.data[p.sentence_count++] = s;
-            sentence_constructor(&s);
-            if (p.sentence_count > 0)
-                doc.data[doc.paragraph_count++] = p;
-            paragraph_constructor(&p);
-        } else {
-            w.data[w_len++] = text[i];
+                word_constructor(&w, &w_len);
+            }
+            skip_this_char = 1;
         }
+        if (text[i] == 0 || text[i] == '.' || text[i] == '\n') {
+            if (s.word_count > 0) {
+                p.data[p.sentence_count++] = s;
+                sentence_constructor(&s);
+            }
+            skip_this_char = 1;
+        }
+        if (text[i] == 0 || text[i] == '\n') {
+            if (p.sentence_count > 0) {
+                doc.data[doc.paragraph_count++] = p;
+                paragraph_constructor(&p);
+            }
+            skip_this_char = 1;
+        }
+        if (text[i] == 0) break;
+        if (!skip_this_char) w.data[w_len++] = text[i];
     }
-    if (w_len > 0)
-        s.data[s.word_count++] = w;
-    if (s.word_count > 0)
-        p.data[p.sentence_count++] = s;
-    if (p.sentence_count > 0)
-        doc.data[doc.paragraph_count++] = p;
     return doc;
 }
 
@@ -107,7 +103,9 @@ struct word kth_word_in_mth_sentence_of_nth_paragraph(struct document Doc, int k
     return kth_sentence_in_mth_paragraph(Doc, m, n).data[k - 1];
 }
 
-void print_word(struct word w) { printf("%s", w.data); }
+void print_word(struct word w) {
+    printf("%s", w.data);
+}
 
 void print_sentence(struct sentence sen) {
     for (int i = 0; i < sen.word_count; i++) {
@@ -133,56 +131,8 @@ void print_document(struct document doc) {
     }
 }
 
-char *get_input_text() {
-    int paragraph_count;
-    scanf("%d", &paragraph_count);
-
-    char p[MAX_PARAGRAPHS][MAX_CHARACTERS], doc[MAX_CHARACTERS];
-    memset(doc, 0, sizeof(doc));
-    getchar();
-    for (int i = 0; i < paragraph_count; i++) {
-        scanf("%[^\n]%*c", p[i]);
-        strcat(doc, p[i]);
-        if (i != paragraph_count - 1)
-            strcat(doc, "\n");
-    }
-
-    char *returnDoc = (char *)malloc((strlen(doc) + 1) * (sizeof(char)));
-    strcpy(returnDoc, doc);
-    return returnDoc;
-}
-
 int main() {
-    char *text          = get_input_text();
-    struct document Doc = get_document(text);
-
-    int q;
-    scanf("%d", &q);
-
-    while (q--) {
-        int type;
-        scanf("%d", &type);
-
-        if (type == 3) {
-            int k, m, n;
-            scanf("%d %d %d", &k, &m, &n);
-            struct word w = kth_word_in_mth_sentence_of_nth_paragraph(Doc, k, m, n);
-            print_word(w);
-        }
-
-        else if (type == 2) {
-            int k, m;
-            scanf("%d %d", &k, &m);
-            struct sentence sen = kth_sentence_in_mth_paragraph(Doc, k, m);
-            print_sentence(sen);
-        }
-
-        else {
-            int k;
-            scanf("%d", &k);
-            struct paragraph para = kth_paragraph(Doc, k);
-            print_paragraph(para);
-        }
-        printf("\n");
-    }
+    char *text = "Learning C is fun.\nMoaz pointers is more fun.It is good to have pointers.";
+    Document d = get_document(text);
+    print_document(d);
 }
