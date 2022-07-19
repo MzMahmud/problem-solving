a81cb26e8852f0374a0c76e6a31800be787a45ca
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-07-19 16:24:35 +0600

    Update Extended GCD.cpp

--- Algorithm Tempates/Extended GCD.cpp
+++ Algorithm Tempates/Extended GCD.cpp
@@ -14,16 +14,13 @@ tuple<int, int, int> egcd(int a, int b) {
 }
 
 int egcd(int a, int b, int &x, int &y) {
-    if (!a) {
-        x = 0;
-        y = 1;
-        return b;
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
     }
-    int x1, y1;
-    int d = egcd(b % a, a, x1, y1);
-
-    x = y1 - (b / a) * x1;
-    y = x1;
+    int x1, y1, d = egcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
     return d;
 }
 
