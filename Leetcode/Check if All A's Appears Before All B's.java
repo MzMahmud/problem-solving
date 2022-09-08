0b0031d61d66d1928a7931b956bdf10a315fffcc
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-09-08 12:56:14 +0600

    Create Check if All A's Appears Before All B's.java

---
+++ Leetcode/Check if All A's Appears Before All B's.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkString(String s) {
+        for(int i = 1;i < s.length();++i) {
+            if('b' == s.charAt(i-1) && 'a' == s.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
