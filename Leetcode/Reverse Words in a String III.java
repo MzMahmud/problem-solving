f0a4c2dc72f659c8daa05a6851859a0755570e94
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-09-22 13:30:08 +0600

    Update Reverse Words in a String III.java

--- Leetcode/Reverse Words in a String III.java
+++ Leetcode/Reverse Words in a String III.java
@@ -1,23 +1,18 @@
-class Solution {
-    private void copyWordInReverse(int startIndexInclusive, int endIndexExclusive,
-                                   String s, StringBuilder reversedWordSentence) {
-        for (int currentIndex = endIndexExclusive + 1;
-             endIndexExclusive < startIndexInclusive;
-             ++currentIndex, --startIndexInclusive
-        ) {
-            reversedWordSentence.setCharAt(currentIndex, s.charAt(startIndexInclusive));
+public class Solution {
+    private void reverseWord(int startIndexInclusive, int endIndexInclusive, StringBuilder reversedWordSentence) {
+        for (; startIndexInclusive <= endIndexInclusive; ++startIndexInclusive, --endIndexInclusive) {
+            char temp = reversedWordSentence.charAt(startIndexInclusive);
+            reversedWordSentence.setCharAt(startIndexInclusive, reversedWordSentence.charAt(endIndexInclusive));
+            reversedWordSentence.setCharAt(endIndexInclusive, temp);
         }
     }
 
-    public String reverseWords(String s) {
-        StringBuilder reversedWordSentence = new StringBuilder(s);
-        for (int lastSpaceIndex = -1, currentIndex = 0; currentIndex <= s.length(); ++currentIndex) {
-            if (currentIndex == s.length() || s.charAt(currentIndex) == ' ') {
-                copyWordInReverse(currentIndex - 1, lastSpaceIndex, s, reversedWordSentence);
-                if (currentIndex < s.length()) {
-                    reversedWordSentence.setCharAt(currentIndex, ' ');
-                    lastSpaceIndex = currentIndex;
-                }
+    public String reverseWords(String sentence) {
+        StringBuilder reversedWordSentence = new StringBuilder(sentence);
+        for (int lastSpaceIndex = -1, currentIndex = 0; currentIndex <= sentence.length(); ++currentIndex) {
+            if (currentIndex == sentence.length() || sentence.charAt(currentIndex) == ' ') {
+                reverseWord(lastSpaceIndex + 1, currentIndex - 1, reversedWordSentence);
+                lastSpaceIndex = currentIndex;
             }
         }
         return reversedWordSentence.toString();
