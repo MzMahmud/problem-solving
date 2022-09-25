d3622bc5d9dbfb5f99942b012421fd958ef35e60
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-09-25 10:16:31 +0600

    Update Rotate Array.java

--- Leetcode/Rotate Array.java
+++ Leetcode/Rotate Array.java
@@ -1,5 +1,23 @@
 class Solution {
+    private void reverse(int[] numbers, int start, int end) {
+        for (; start <= end; ++start, --end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+        }
+    }
+
     public void rotate(int[] numbers, int k) {
+        int n = numbers.length;
+        if (n == 0 || (k = k % n) == 0) {
+            return;
+        }
+        reverse(numbers, 0, n - 1);
+        reverse(numbers, 0, k - 1);
+        reverse(numbers, k, n - 1);
+    }
+    
+    public void rotateOrderNSpace(int[] numbers, int k) {
         int n = numbers.length;
         if (n == 0 || (k = k % n) == 0) {
             return;
