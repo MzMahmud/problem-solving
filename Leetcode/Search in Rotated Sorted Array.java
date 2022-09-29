bc099df515a3b4484a896da7498e2e6ee99752f2
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-09-29 10:51:39 +0600

    Create Search in Rotated Sorted Array.java

---
+++ Leetcode/Search in Rotated Sorted Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    /* Notice that in any rotated array a,
+     * if a[lo] <= a[mid] -> a[lo...mid] is sorted
+     * else               -> a[hi...mid] is sorted
+     * Now if a[lo...mid] is sorted and target is not in the interval [a[lo], a[hi]]
+     *        then target can not be in that part of the array. This eliminates half of the array.
+     * Similarly, if target is not in the interval [a[mid], a[lo]] eliminates that part of the array.
+     * This cuts the input size by half everytime, which leads to O(lg n) time.
+     **/
+    public int search(int[] rotatedSortedArray, int target) {
+        int lo = 0, hi = rotatedSortedArray.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (rotatedSortedArray[mid] == target) {
+                return mid;
+            }
+            if (rotatedSortedArray[lo] <= rotatedSortedArray[mid]) {
+                if (isBetween(target, rotatedSortedArray[lo], rotatedSortedArray[mid])) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (isBetween(target, rotatedSortedArray[mid], rotatedSortedArray[hi])) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBetween(int target, int low, int high) {
+        return (low <= target) && (target <= high);
+    }
+}
