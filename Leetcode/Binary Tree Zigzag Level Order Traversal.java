4c001ed6aff6773b6387bd1040829562abb1ddf7
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2022-11-09 11:46:23 +0600

    Create Binary Tree Zigzag Level Order Traversal.java

---
+++ Leetcode/Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        List<List<Integer>> zigzagLevels = new ArrayList<>();
+        boolean doReverse = false;
+        while (!queue.isEmpty()) {
+            int nNodesInLevel = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < nNodesInLevel; ++i) {
+                TreeNode treeNode = queue.poll();
+                level.add(treeNode.val);
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+            if(doReverse) {
+                Collections.reverse(level);
+            }
+            zigzagLevels.add(level);
+            doReverse = !doReverse;
+        }
+        return zigzagLevels;
+    }
+}
