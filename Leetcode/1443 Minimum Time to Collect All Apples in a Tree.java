98d5802a9d6a8d6940772f579e1d57d9669d388e
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-01-11 12:35:28 +0600

    Update 1443 Minimum Time to Collect All Apples in a Tree.java

--- Leetcode/1443 Minimum Time to Collect All Apples in a Tree.java
+++ Leetcode/1443 Minimum Time to Collect All Apples in a Tree.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 class Solution {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         Map<Integer, List<Integer>> childrenByNode = new HashMap<>();
@@ -29,5 +31,45 @@ class Solution {
             }
         }
         return time;
+    }
+}
+
+class FunctionalSolution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        Map<Integer, List<Integer>> childrenByNode =
+                Arrays.stream(edges)
+                      .map(FunctionalSolution::getBidirectionalEdges)
+                      .flatMap(Collection::stream)
+                      .collect(Collectors.groupingBy(
+                              FunctionalSolution::getFromNodeIndex,
+                              Collectors.mapping(FunctionalSolution::getToNodeIndex, Collectors.toList())
+                      ));
+        Integer timeToGetApple = minTimeToGetApple(0, -1, childrenByNode, hasApple);
+        return timeToGetApple == null ? 0 : timeToGetApple;
+    }
+
+    private Integer minTimeToGetApple(int root, int parent, Map<Integer, List<Integer>> childrenByNode, List<Boolean> hasApple) {
+        int childMinTime =
+                childrenByNode.getOrDefault(root, List.of())
+                              .stream()
+                              .filter(child -> child != parent)
+                              .map(child -> minTimeToGetApple(child, root, childrenByNode, hasApple))
+                              .filter(Objects::nonNull)
+                              .mapToInt(minTime -> minTime + 2)
+                              .sum();
+        boolean hasAnyApples = hasApple.get(root) || childMinTime > 0;
+        return hasAnyApples ? childMinTime : null;
+    }
+
+    private static Integer getFromNodeIndex(int[] edge) {
+        return edge[0];
+    }
+
+    private static Integer getToNodeIndex(int[] edge) {
+        return edge[1];
+    }
+
+    private static List<int[]> getBidirectionalEdges(int[] edge) {
+        return List.of(edge, new int[]{edge[1], edge[0]});
     }
 }
