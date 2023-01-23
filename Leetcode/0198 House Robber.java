b2dd4d0a430708e30e452a6645e680939c05b1ed
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-01-23 13:38:21 +0600

    Update 0198 House Robber.java

--- Leetcode/0198 House Robber.java
+++ Leetcode/0198 House Robber.java
@@ -1,15 +1,26 @@
 class Solution {
+    // optimized DP Solution
     public int rob(int[] nums) {
-        return rob(0, nums, new Integer[nums.length]);
+        int lootN = 0, lootN_2 = 0, lootN_1 = 0;
+        for (int num : nums) {
+            lootN = Math.max(lootN_1, num + lootN_2);
+            lootN_2 = lootN_1;
+            lootN_1 = lootN;
+        }
+        return lootN;
     }
 
-    private int rob(int index, int[] nums, Integer[] cache) {
+    public int robDpSolution(int[] nums) {
+        return robDpSolution(0, nums, new Integer[nums.length]);
+    }
+
+    private int robDpSolution(int index, int[] nums, Integer[] cache) {
         if (index >= nums.length) {
             return 0;
         }
         if (cache[index] == null) {
-            int dontTakeIndex = rob(index + 1, nums, cache);
-            int takeIndex = nums[index] + rob(index + 2, nums, cache);
+            int dontTakeIndex = robDpSolution(index + 1, nums, cache);
+            int takeIndex = nums[index] + robDpSolution(index + 2, nums, cache);
             cache[index] = Math.max(dontTakeIndex, takeIndex);
         }
         return cache[index];
