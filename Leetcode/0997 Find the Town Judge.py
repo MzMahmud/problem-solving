dc9913f92d21d5b89b9df192c520c2f2c5a3f158
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-01-23 10:31:27 +0600

    Update 0997 Find the Town Judge.py

--- Leetcode/0997 Find the Town Judge.py
+++ Leetcode/0997 Find the Town Judge.py
@@ -1,12 +1,12 @@
 class Solution:
     def findJudge(self, n, edges):
-        in_minus_degree = [0] * (n + 1)
+        in_minus_out_degree = [0] * (n + 1)
         for u, v in edges:
-            in_minus_degree[u] -= 1
-            in_minus_degree[v] += 1
-            
+            in_minus_out_degree[u] -= 1
+            in_minus_out_degree[v] += 1
+
         for i in range(1, n + 1):
-            if in_minus_degree[i] == (n - 1):
+            if in_minus_out_degree[i] == (n - 1):
                 return i
-            
+
         return -1
