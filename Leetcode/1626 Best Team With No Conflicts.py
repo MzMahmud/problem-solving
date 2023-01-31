bc15469c8566961e7ac1188546ccf757dbbe6b61
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-01-31 13:34:00 +0600

    leecode 1626 python solution refactored

--- Leetcode/1626 Best Team With No Conflicts.py
+++ Leetcode/1626 Best Team With No Conflicts.py
@@ -1,15 +1,25 @@
 class Solution:
     def bestTeamScore(self, scores, ages):
-        score_ages = list(zip(ages, scores))
+        score_ages = [ScoreAge(age, score) for age, score in zip(ages, scores)]
         score_ages.sort()
 
-        best_scores = [score for _, score in score_ages]
+        best_scores = [score_age.score for score_age in score_ages]
         max_best_scores = float("-inf")
+        
         for i in range(len(best_scores)):
             for j in range(i - 1, -1, -1):
-                if score_ages[i][1] >= score_ages[j][1]:
-                    best_scores[i] = max(best_scores[i], score_ages[i][1] + best_scores[j])
+                if score_ages[j].score <= score_ages[i].score:
+                    best_scores[i] = max(best_scores[i], score_ages[i].score + best_scores[j])
 
             max_best_scores = max(max_best_scores, best_scores[i])
 
         return max_best_scores
+
+
+class ScoreAge:
+    def __init__(self, age, score):
+        self.age = age
+        self.score = score
+
+    def __lt__(self, other):
+        return (self.age, self.score) < (other.age, other.score)
