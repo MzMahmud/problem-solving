1d387cc38b6560a5db04d644131514ad9fc7e0d2
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-01-31 21:57:36 +0600

    simplified leetcode 0054 slution

--- Leetcode/0054 Spiral Matrix.java
+++ Leetcode/0054 Spiral Matrix.java
@@ -1,50 +1,33 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        int totalElements = matrix.length * matrix[0].length;
-        List<Integer> spiral = new ArrayList<>();
-        int directionIndex = 0;
-        int[] position = {0, 0}; 
-        while(spiral.size() < totalElements) {
-            if(isValidPosition(position, matrix)) {
-                spiral.add(markVisitedAndGetValue(matrix, position));
-            }
-            var nextPosition = getPosition(position, DIRECTIONS[directionIndex]);
-            if(isValidPosition(nextPosition, matrix)) {
-                position = nextPosition;
-            } else {
-                directionIndex = (directionIndex + 1) % DIRECTIONS.length;
-            }
-        }
-        return spiral;
-    }
-    
-    private static final int[][] DIRECTIONS = {
-        { 0, 1},
-        { 1, 0},
-        { 0,-1},
-        {-1, 0}
+    private static final int[][] DIRS = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0},
     };
 
-    private static final int VISITED_MARKER = 101;
+    private static final int VISITED = 101;
 
-    private static int[] getPosition(int[] position, int[] direction) {
-        return new int[] {
-            position[0] + direction[0],
-            position[1] + direction[1],
-        };
-    }
-
-    private static boolean isValidPosition(int[] position, int[][] matrix) {
-        int i = position[0], j = position[1];
-        return (0 <= i && i < matrix.length)
-               && (0 <= j && j < matrix[0].length)
-               && (matrix[i][j] != VISITED_MARKER);
-    }
-
-    public static int markVisitedAndGetValue(int[][] matrix, int[] position) {
-        int i = position[0], j = position[1];
-        int value = matrix[i][j];
-        matrix[i][j] = VISITED_MARKER;
-        return value;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> spiral = new ArrayList<>();
+        int n = matrix.length, m = matrix[0].length, number = 1;
+        int elements = n * m, dirIndex = 0;
+        int i = 0, j = 0;
+        while (number <= elements) {
+            spiral.add(matrix[i][j]);
+            matrix[i][j] = VISITED;
+            int nextI = i + DIRS[dirIndex][0], nextJ = j + DIRS[dirIndex][1];
+            boolean isInvalid = 
+                nextI < 0 || nextI >= n 
+                || nextJ < 0 || nextJ >= m 
+                || matrix[nextI][nextJ] == VISITED;
+            if (isInvalid) {
+                dirIndex = (dirIndex + 1) % DIRS.length;
+            }
+            i += DIRS[dirIndex][0];
+            j += DIRS[dirIndex][1];
+            ++number;
+        }
+        return spiral;
     }
 }
