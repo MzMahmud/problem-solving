97265ebab0a3dac3d75e9f155e762193b1ab0ed6
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-02-03 21:25:51 +0600

    solved leetcode 0043 in O(mn) time O(m+n) space

--- Leetcode/0043 Multiply Strings.py
+++ Leetcode/0043 Multiply Strings.py
@@ -4,12 +4,39 @@ from functools import reduce
 
 class Solution:
     def multiply(self, a, b):
-        return multiply_classic_math(a, b)
+        return multiply_optimized(a, b)
+
+
+# O(m * n) time, O(m + n) space
+def multiply_optimized(a, b):
+    if a == '0' or b == 0:
+        return '0'
+
+    m, n = len(a), len(b)
+    product = [0 for _ in range(m + n)]
+    last_index = len(product) - 1
+
+    for i, ai in enumerate(reversed(a)):
+        ai = int(ai)
+        for bi in reversed(b):
+            multiplied = product[i] + ai * int(bi)
+            product[i] = multiplied % 10
+
+            if i < last_index:
+                product[i+1] += multiplied // 10
+
+            i += 1
+
+    while product[-1] == 0:
+        product.pop()
+
+    return "".join(map(str, reversed(product)))
 
 
 # uses the algorithm we use to mutiply by hand
 # m = length a, n = length b
 # overall time O(2 * m * (m + n) + m + n) = O(m^2 + mn)
+# O(m^2 + mn) space
 def multiply_classic_math(a, b):
     if a == '0' or b == '0':
         return '0'
@@ -17,7 +44,7 @@ def multiply_classic_math(a, b):
     sub_products = []
     # runs m time
     for index, ai in enumerate(reversed(a)):
-        # takes O(m + n) time
+        # takes O(m + n) time and space
         sub_product = multiply_single_digit(ai, reversed(b), index)
         sub_products.append(sub_product)
 
