0111f39deb308e3ee5adba834c388153f6d5638d
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-02-05 09:20:33 +0600

    updated leetcode 0438 with dictionary solution

--- Leetcode/0438 Find All Anagrams in a String.py
+++ Leetcode/0438 Find All Anagrams in a String.py
@@ -1,50 +1,47 @@
-ALPHABET_SIZE = 26
-
-
 class Solution:
     def findAnagrams(self, s, p):
         len_s, len_p = len(s), len(p)
         if len_s < len_p:
             return []
 
-        p_counts = [0 for _ in range(ALPHABET_SIZE)]
-        s_window_counts = [0 for _ in range(ALPHABET_SIZE)]
+        p_counts = {}
+        s_window_counts = {}
         for char_p, char_s in zip(p, s):
-            p_counts[char_index(char_p)] += 1
-            s_window_counts[char_index(char_s)] += 1
+            p_counts[char_p] = p_counts.get(char_p, 0) + 1
+            s_window_counts[char_s] = s_window_counts.get(char_s, 0) + 1
 
         matches = 0
-        for p_count, s_window_count in zip(p_counts, s_window_counts):
-            if p_count == s_window_count:
-                matches += 1
+        for char_p in p_counts:
+            matches = get_updated_matches(char_p, p_counts, s_window_counts, matches, 1)
 
+        len_p_counts = len(p_counts)
         anagram_indices = []
-        if matches == ALPHABET_SIZE:
+        if matches == len_p_counts:
             anagram_indices.append(0)
 
         for i in range(len_p, len_s):
-            enter_char_index = char_index(s[i])
-            exit_char_index = char_index(s[i - len_p])
+            enter_char, exit_char = s[i], s[i - len_p]
 
-            if enter_char_index != exit_char_index:
-                if p_counts[enter_char_index] == s_window_counts[enter_char_index]:
-                    matches -= 1
-                if p_counts[exit_char_index] == s_window_counts[exit_char_index]:
-                    matches -= 1
+            if enter_char != exit_char:
+                matches = get_updated_matches(enter_char, p_counts, s_window_counts, matches, -1)
+                matches = get_updated_matches(exit_char, p_counts, s_window_counts, matches, -1)
 
-                s_window_counts[enter_char_index] += 1
-                s_window_counts[exit_char_index] -= 1
+                s_window_counts[enter_char] = s_window_counts.get(enter_char, 0) + 1
+                s_window_counts[exit_char] -= 1
 
-                if p_counts[enter_char_index] == s_window_counts[enter_char_index]:
-                    matches += 1
-                if p_counts[exit_char_index] == s_window_counts[exit_char_index]:
-                    matches += 1
+                matches = get_updated_matches(enter_char, p_counts, s_window_counts, matches, 1)
+                matches = get_updated_matches(exit_char, p_counts, s_window_counts, matches, 1)
 
-            if matches == ALPHABET_SIZE:
+            if matches == len_p_counts:
                 anagram_indices.append(i - len_p + 1)
 
         return anagram_indices
 
 
-def char_index(char):
-    return ord(char) - ord('a')
+def get_updated_matches(char, counts_1, counts_2, matches, increment):
+    char_count_mathced = (
+        char in counts_1
+        and char in counts_2
+        and counts_1[char] == counts_2[char]
+    )
+    return matches + increment if char_count_mathced else matches
