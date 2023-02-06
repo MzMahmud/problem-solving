358972d2442105038fcb6ab0c633c6b038410d3c
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-02-06 09:37:56 +0600

    solved leetcode 1470 in O(1) space

--- Leetcode/1470 Shuffle the Array.py
+++ Leetcode/1470 Shuffle the Array.py
@@ -1,13 +1,26 @@
 class Solution:
+    # O(n) time, O(1) space
     def shuffle(self, nums, n):
-        shuffled = list(nums)
-        
-        i, j, k = 0, n, 0
-        while j < len(nums):
-            shuffled[k] = nums[i]
-            shuffled[k + 1] = nums[j]
-            i += 1
-            j += 1
-            k += 2
+        length = 2 * n
 
-        return shuffled
+        for start in range(1, n):
+            # if number is visited skip this start position
+            if nums[start] < 0:
+                continue
+
+            prev_value, curr = nums[start], 2 * start
+            while True:
+                curr_nums = nums[curr]
+                # as 1 <= nums[i] <= 10^3 put a negative number to indicate that this position is already visited
+                nums[curr] = -prev_value 
+                prev_value = curr_nums
+
+                if curr == start:
+                    break
+
+                curr += curr if curr < n else curr - length + 1
+
+        for i in range(length):
+            nums[i] = abs(nums[i])
+
+        return nums
