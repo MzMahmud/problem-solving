503b25dd6ad0c7fcbe6d5d033f58bc023e9f2244
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2023-03-06 11:13:43 +0600

    solved leetcode 1539 in log n time

--- Leetcode/1539 Kth Missing Positive Number.py
+++ Leetcode/1539 Kth Missing Positive Number.py
@@ -1,4 +1,21 @@
 class Solution:
+    # O(log n) time, O(1) space
+    def findKthPositive(self, array, k):
+        last_missing, n_missing = 1, 0
+        
+        lo, hi = 0, len(array) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            new_n_missing = array[mid] - mid - 1
+            if new_n_missing < k:
+                lo = mid + 1
+                n_missing = new_n_missing
+                last_missing = array[mid] + 1
+            else:
+                hi = mid - 1
+
+        return last_missing + k - n_missing - 1
+
     # O(n) time, O(1) space
     def findKthPositive(self, array, k):
         last_missing, n_missing = 1, 0
--- Leetcode/1539 Kth Missing Positive Number.rs
+++ Leetcode/1539 Kth Missing Positive Number.rs
@@ -1,4 +1,23 @@
 impl Solution {
+    // O(log n) time, O(1) space
+    pub fn find_kth_positive(array: Vec<i32>, k: i32) -> i32 {
+        let (mut last_missing, mut n_missing) = (1, 0);
+        let (mut lo, mut hi) = (0, array.len() as i32 - 1);
+        while lo <= hi {
+            let mid = lo + (hi - lo) / 2;
+            let n = array[mid as usize];
+            let new_n_missing = n - mid - 1;
+            if new_n_missing < k {
+                lo = mid + 1;
+                n_missing = new_n_missing;
+                last_missing = n + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        last_missing + k - n_missing - 1
+    }
+
     // O(n) time, O(1) space
     pub fn find_kth_positive(array: Vec<i32>, k: i32) -> i32 {
         let mut last_missing = 1;
