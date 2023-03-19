d429d4c0cc07533d31ff2fd0e14112a0f4661e4d
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-03-19 10:41:17 +0600

    Update 0211 Design Add and Search Words Data Structure.ts

--- Leetcode/0211 Design Add and Search Words Data Structure.ts
+++ Leetcode/0211 Design Add and Search Words Data Structure.ts
@@ -1,51 +1,38 @@
-type WordDictNode = {
-    isEndOfWord: boolean;
-    children: Map<string, WordDictNode>;
+class WordDictNode {
+    isEndOfWord: boolean = false;
+    children: Map<string, WordDictNode> = new Map();
 }
 
 class WordDictionary {
-    root: WordDictNode;
-
-    constructor() {
-        this.root = {
-            isEndOfWord: false,
-            children: new Map()
-        }
-    }
+    root: WordDictNode = new WordDictNode();
 
     addWord(word: string): void {
         let curr = this.root;
         for(const char of word) {
             if(!curr.children.has(char)) {
-                curr.children.set(
-                    char, {
-                    isEndOfWord: false,
-                    children: new Map()
-                });
+                curr.children.set(char, new WordDictNode());
             }
             curr = curr.children.get(char);
         }
         curr.isEndOfWord = true;
     }
 
-    searchFromIndex(word: string, index: number, root: WordDictNode): boolean {
+    search(word: string, index: number = 0, root: WordDictNode = this.root): boolean {
         if(index == word.length) {
             return root.isEndOfWord;
         }
-        const char = word[index];
-        if(char === '.') {
-            for(const child of root.children.values()) {
-                if(this.searchFromIndex(word, index + 1, child)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        const child = root.children.get(char);
-        return child != null && this.searchFromIndex(word, index + 1, child);
-    }
 
-    search(word: string): boolean {
-        return this.searchFromIndex(word, 0, this.root);
+        const char = word[index];
+        if(char !== '.') {
+            const child = root.children.get(char);
+            return child != null && this.search(word, index + 1, child);
+        }
+
+        for(const child of root.children.values()) {
+            if(this.search(word, index + 1, child)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
