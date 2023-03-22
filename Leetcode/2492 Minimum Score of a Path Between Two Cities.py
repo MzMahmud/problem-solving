62bd71c61182f55376202be2473f75551f6e9479
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-03-22 12:19:50 +0600

    solved leetcode 2492 with dfs

--- Leetcode/2492 Minimum Score of a Path Between Two Cities.py
+++ Leetcode/2492 Minimum Score of a Path Between Two Cities.py
@@ -6,6 +6,26 @@ class Solution:
             adj_list[dst].append((src, weight))
 
         return bfs(n, adj_list)
+    
+     def minScore(self, n, roads):
+        adj_list = [[] for _ in range(n + 1)]
+        for src, dst, weight in roads:
+            adj_list[src].append((dst, weight))
+            adj_list[dst].append((src, weight))
+        visited = [False] * (n + 1)
+        self.min_edge_weight = float('inf')
+        self.dfs(n, adj_list, visited)
+        return self.min_edge_weight
+
+    def dfs(self, start, adj_list, visited):
+        if visited[start]:
+            return
+        
+        visited[start] = True
+    
+        for child, weight in adj_list[start]:
+            self.min_edge_weight = min(self.min_edge_weight, weight)
+            self.dfs(child, adj_list, visited)
 
 
 def bfs(n, adj_list):
