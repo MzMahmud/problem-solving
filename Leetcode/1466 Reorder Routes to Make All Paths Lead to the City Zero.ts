26343f71c14cbea54de2957fde48f4adbd90ecbc
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-03-24 11:08:43 +0600

    refactored leetcode 1466 ts solution

--- Leetcode/1466 Reorder Routes to Make All Paths Lead to the City Zero.ts
+++ Leetcode/1466 Reorder Routes to Make All Paths Lead to the City Zero.ts
@@ -1,24 +1,21 @@
-const [FORWARD, BACKWARD] = [1, -1];
+const [FORWARD, BACKWARD] = [1, 0];
 
 function minReorder(n: number, connections: number[][]): number {
-    const adjListWithDirection: [number, number][][] = new Array(n);
+    const adjList: [number, number][][] = new Array(n);
     for (let i = 0; i < n; i++) {
-        adjListWithDirection[i] = [];
+        adjList[i] = [];
     }
     for (const [src, dst] of connections) {
-        adjListWithDirection[src].push([dst, FORWARD]);
-        adjListWithDirection[dst].push([src, BACKWARD]);
+        adjList[src].push([dst, FORWARD]);
+        adjList[dst].push([src, BACKWARD]);
     }
     let nParentToChildEdges = 0;
     const dfs = (root: number, parent: number) => {
-        for (const [child, edgeDirection] of adjListWithDirection[root]) {
-            if (child === parent) {
-                continue;
+        for (const [child, direction] of adjList[root]) {
+            if (child !== parent) {    
+                nParentToChildEdges += direction;
+                dfs(child, root);
             }
-            if (edgeDirection == FORWARD) {
-                nParentToChildEdges++;
-            }
-            dfs(child, root);
         }
     }
     dfs(0, -1);
