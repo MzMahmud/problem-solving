54b3a94960aeb164060a232f0a04e938f66e4751
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-01 10:13:09 +0600

    solved leetcode 704 with ts

---
+++ Leetcode/0704 Binary Search.ts
@@ -0,0 +1,15 @@
+function search(nums: number[], target: number): number {
+    let lo = 0, hi = nums.length - 1;
+    while(lo <= hi) {
+        const mid = lo + Math.floor((hi - lo) / 2);
+        if(nums[mid] == target) {
+            return mid;
+        }
+        if(nums[mid] < target) {
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    return -1;
+}
