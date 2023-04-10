ad8b6f125a9dbeaf42ac8645947d982f6b991efe
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-10 15:42:46 +0600

    Update 0020 Valid Parentheses.py

--- Leetcode/0020 Valid Parentheses.py
+++ Leetcode/0020 Valid Parentheses.py
@@ -6,7 +6,6 @@ class Solution:
         for char in s:
             if char in ['(', '{', '[']:
                 stack.append(char)
-                continue 
-            if len(stack) == 0 or stack.pop() != right_to_left_map[char]:
+            elif len(stack) == 0 or stack.pop() != right_to_left_map[char]:
                 return False
         return len(stack) == 0
