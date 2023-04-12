f196739a04ae238097d6ba16d94c521e420e74b8
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-12 15:21:49 +0600

    solved leetcode 2623

---
+++ Leetcode/2623 Memoize.ts
@@ -0,0 +1,24 @@
+type Fn = (...params: any) => any
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map<string, any>();
+    return function(...args) {
+        const key = `${args}`;
+        if(!cache.has(key)) {
+            cache.set(key, fn(...args));
+        }
+        return cache.get(key);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
