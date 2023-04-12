e996e37579259797fdf0805024e4d6f8e958f233
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-12 13:39:01 +0600

    Update 2619 Array Prototype Last.ts

--- Leetcode/2619 Array Prototype Last.ts
+++ Leetcode/2619 Array Prototype Last.ts
@@ -1,1 +1,15 @@
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
 
+Array.prototype.last = function() {
+    const lastElem = this.at(this.length - 1);
+    return lastElem === undefined? -1: lastElem;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
