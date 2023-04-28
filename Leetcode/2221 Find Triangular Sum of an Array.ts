b8aaa7b3e227e9480643da8f3ece115ecf982ed6
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-29 00:41:06 +0600

    Update 2221 Find Triangular Sum of an Array.ts

--- Leetcode/2221 Find Triangular Sum of an Array.ts
+++ Leetcode/2221 Find Triangular Sum of an Array.ts
@@ -38,3 +38,51 @@ function triangularSum(nums: number[]): number {
     }
     return nums[0];
 }
+                                
+// modular inverse solution O(n)
+const modInverse10 = [0, 1, 0, 7, 0, 0, 0, 3, 0, 9];
+const exp2Mod10 = [6, 2, 4, 8];
+
+function triangularSum(nums: number[]): number {
+    const m = nums.length - 1;
+    let mCk = 1, exp2 = 0, exp5 = 0;   
+    let lastDigit = 0;
+    for (let k = 0; k <= m; k++) {
+        if (exp2 === 0 || exp5 === 0) {
+            let binom = mCk;
+            if (exp2 !== 0) {
+                binom = mCk * exp2Mod10[exp2 % 4] % 10;
+            } else if (exp5 !== 0) {
+                binom = mCk * 5 % 10;
+            }
+            lastDigit = (lastDigit + binom * nums[k]) % 10;
+        }
+        if (k == m) {
+            continue;
+        }
+        // mCk *= m - k
+        let mul = m - k;
+        while (mul % 2 == 0) {
+            mul /= 2;
+            exp2++;
+        }
+        while (mul % 5 == 0) {
+            mul /= 5;
+            exp5++;
+        }
+        mCk = mCk * mul % 10;
+
+        // mCk /= k + 1
+        let div = k + 1;
+        while (div % 2 == 0) {
+            div /= 2;
+            exp2--;
+        }
+        while (div % 5 == 0) {
+            div /= 5;
+            exp5--;
+        }
+        mCk = mCk * modInverse10[div % 10] % 10;
+    }
+    return lastDigit;
+}
