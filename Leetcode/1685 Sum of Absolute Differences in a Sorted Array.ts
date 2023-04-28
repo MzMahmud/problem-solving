e9760364bcbc47e0ebc5d7e191ecbc507c13793c
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-04-28 19:22:53 +0600

    leetcode 1685 memory optimized

--- Leetcode/1685 Sum of Absolute Differences in a Sorted Array.ts
+++ Leetcode/1685 Sum of Absolute Differences in a Sorted Array.ts
@@ -1,15 +1,14 @@
 function getSumAbsoluteDifferences(nums: number[]): number[] {
     const n = nums.length;
-    const suffixSum = new Array<number>(n + 1);
-    suffixSum[n] = 0;
-    for (let i = n - 1; i >= 0; i--) {
-        suffixSum[i] = nums[i] + suffixSum[i + 1];
+    const sums = new Array<number>(n);
+    sums[n - 1] = nums[n - 1];
+    for (let i = n - 2; i >= 0; i--) {
+        sums[i] = nums[i] + sums[i + 1];
     }
     let prefixSum = 0;
-    const sums = new Array<number>(n);
     for (let i = 0; i < n; i++) {
         prefixSum += nums[i];
-        sums[i] = (2 * i + 2 - n) * nums[i] - prefixSum + suffixSum[i + 1];
+        sums[i] += (2 * i + 1 - n) * nums[i] - prefixSum;
     }
     return sums;
 }
