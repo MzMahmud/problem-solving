8d823c14a728d758b51e563fa3b7460bae7eed05
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-05-08 14:28:34 +0600

    Create 2665 Counter II.ts

---
+++ Leetcode/2665 Counter II.ts
@@ -0,0 +1,29 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+class Counter implements ReturnObj {
+    private curr: number;
+
+    constructor(private init: number) {
+        this.curr = this.init;
+    }
+    
+    public increment(): number {
+        return ++this.curr;
+    }
+    
+    public decrement(): number {
+        return --this.curr;
+    }
+    
+    public reset(): number {
+        return this.curr = this.init;
+    }
+}
+
+function createCounter(init: number): ReturnObj {
+    return new Counter(init);
+}
