e2b8fed9823bc140a09679de8bdb1d9ea390cbf4
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-05-08 15:49:03 +0600

    Create 2666 Allow One Function Call.ts

---
+++ Leetcode/2666 Allow One Function Call.ts
@@ -0,0 +1,9 @@
+function once<T extends (...args: any[]) => any>(fn: T): ((...args: Parameters<T>) => ReturnType<T> | undefined) {
+    let called = false;
+    return function (...args) {
+        if (!called) {
+            called = true;
+            return fn(...args);
+        }
+    };
+}
