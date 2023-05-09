724282eb313138afbac50d91253c376ae38bac4b
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-05-09 16:53:41 +0600

    Create 0100 Same Tree.ts

---
+++ Leetcode/0100 Same Tree.ts
@@ -0,0 +1,8 @@
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p === null || q === null) {
+        return p === q;
+    }
+    return p.val === q.val 
+        && isSameTree(p.left, q.left)
+        && isSameTree(p.right, q.right);
+}
