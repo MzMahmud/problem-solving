4d9ed6395c5756c11132c15be87f49681cd10c0c
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-05-09 10:41:22 +0600

    Create 1662 Check If Two String Arrays are Equivalent.ts

---
+++ Leetcode/1662 Check If Two String Arrays are Equivalent.ts
@@ -0,0 +1,15 @@
+function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
+    let i1 = 0, j1 = 0, i2 = 0, j2 = 0;
+    while (i1 < word1.length && i2 < word2.length) {
+        if (word1[i1][j1++] !== word2[i2][j2++]) {
+            return false;
+        }
+        if (j1 === word1[i1].length) {
+            i1++, j1 = 0;
+        }
+        if (j2 === word2[i2].length) {
+            i2++, j2 = 0;
+        }
+    }
+    return i1 === word1.length && i2 === word2.length;
+}
