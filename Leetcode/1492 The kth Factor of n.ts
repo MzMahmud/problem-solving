5dcf7211469141fc0ec0dc4fb72576c3e6adb861
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2023-05-26 16:35:36 +0600

    Update 1492 The kth Factor of n.ts

--- Leetcode/1492 The kth Factor of n.ts
+++ Leetcode/1492 The kth Factor of n.ts
@@ -1,3 +1,23 @@
+// O(sqrt n) time, O(1) space
+function kthFactor(n: number, k: number): number {
+    const sqrt = Math.floor(Math.sqrt(n));
+    for (let d = 1; d <= sqrt; d++) {
+        if (n % d === 0 && --k === 0) {
+            return d;
+        }
+    }
+    for (let d = sqrt; d >= 1; d--) {
+        if (n % d === 0) {
+            const otherD = n / d;
+            if (d != otherD && --k === 0) {
+                return otherD;
+            }
+        }
+    }
+    return -1;
+}
+
+// O(sqrt n) time, O(sqrt n) space
 function kthFactor(n: number, k: number): number {
     const low: number[] = [], high: number[] = [];
     for (let d = 1; d * d <= n; d++) {
