d991fef5ab1927458d712df0576f871b90e771ed
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2024-07-02 12:17:34 +0600

    Update 0350 Intersection of Two Arrays II.ts

--- Leetcode/0350 Intersection of Two Arrays II.ts
+++ Leetcode/0350 Intersection of Two Arrays II.ts
@@ -1,21 +1,14 @@
 function intersect(a: number[], b: number[]): number[] {
-    const bCounter = counter(b);
-    const intersec = [];
-    for (const ai of a) {
-        const bCount = bCounter.get(ai) ?? 0;
-        if (bCount !== 0) {
-            intersec.push(ai);
-            bCounter.set(ai, bCount - 1);
+    const count: Record<number, number> = {};
+    for (const n of a) {
+        count[n] = (count[n] ?? 0) + 1; 
+    }
+    const intersection: number[] = [];
+    for (const n of b) {
+        if (count[n]) {
+            intersection.push(n);
+            count[n]--;
         }
     }
-    return intersec;
-}
-
-function counter(a: number[]) {
-    const counts = new Map<number, number>();
-    for (const ai of a) {
-        const count = (counts.get(ai) ?? 0) + 1;
-        counts.set(ai, count);
-    }
-    return counts;
+    return intersection;
 }
