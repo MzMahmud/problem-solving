b2cf7de3ce0afda1f825bec70adc234691877665
Moaz Mahmud <1505064.mm@ugrad.cse.buet.ac.bd>
2024-08-27 20:12:07 +0600

    Update 0726 Number of Atoms.ts

--- Leetcode/0726 Number of Atoms.ts
+++ Leetcode/0726 Number of Atoms.ts
@@ -1,49 +1,43 @@
-function countOfAtoms(s: string) {
-    const n = s.length;
-    const atomCounts = [new Map<string, number>()];
-    for (let i = 0; i < n;) {
-        if (s[i] === "(") {
-            atomCounts.push(new Map());
-            i++;
-        } else if (s[i] === ")") {
-            const { num, nextI } = getNumber(s, i + 1);
-            const atomCount = atomCounts.pop()!;
-            const prevAtomCounts = atomCounts.at(-1)!;
-            for (const [atom, count] of atomCount) {
-                const updated = count * num + (prevAtomCounts.get(atom) ?? 0);
-                prevAtomCounts.set(atom, updated);
-            }
-            i = nextI;
-        } else {
-            const atomCount = atomCounts.at(-1)!;
-            let atom = s[i++];
-            while (i < n && isLower(s[i])) atom += s[i++];
-            const { num, nextI } = getNumber(s, i);
-            const count = (atomCount.get(atom) ?? 0) + num;
-            atomCount.set(atom, count);
-            i = nextI;
-        }
-    }
-    return [...atomCounts[0].entries()]
+function countOfAtoms(formula: string) {
+    const { atomCount } = countAtoms(formula, 0);
+    return [...atomCount.entries()]
         .sort()
-        .map(([atom, count]) => `${atom}${count == 1 ? "" : count}`)
+        .map(([atom, count]) => count == 1 ? atom : `${atom}${count}`)
         .join("");
 }
 
-function getNumber(s: string, i: number) {
-    let num = 0;
-    while (i < s.length && isDigit(s[i])) {
-        num = num * 10 + Number(s[i]);
-        i++;
+function countAtoms(formula: string, i: number) {
+    const atomCount = new Map<string, number>();
+    const n = formula.length;
+    while (i < n) {
+        const char = formula[i];
+        if (char == "(") {
+            const { atomCount: innerCount, nextI } = countAtoms(formula, i + 1);
+            i = nextI;
+            for (const [atom, count] of innerCount) {
+                const newCount = (atomCount.get(atom) ?? 0) + count;
+                atomCount.set(atom, newCount);
+            }
+        } else if (char == ")") {
+            i++;
+            let num = 0;
+            while (i < n && isDigit(formula[i])) num = num * 10 + Number(formula[i++]);
+            if (num == 0) num = 1;
+            for (const [atom, count] of atomCount) atomCount.set(atom, count * num);
+            break;
+        } else {
+            i++;
+            let atom = char;
+            while (i < n && isLower(formula[i])) atom += formula[i++];
+            let count = 0;
+            while (i < n && isDigit(formula[i])) count = count * 10 + Number(formula[i++]);
+            if (count == 0) count = 1;
+            count += (atomCount.get(atom) ?? 0);
+            atomCount.set(atom, count);
+        }
     }
-    if (num == 0) num = 1;
-    return { num, nextI: i };
+    return { atomCount, nextI: i };
 }
 
-function isDigit(s: string) {
-    return /^[0-9]$/.test(s);
-}
-
-function isLower(s: string) {
-    return /^[a-z]$/.test(s);
-}
+const isDigit = (s: string) => /^[0-9]$/.test(s);
+const isLower = (s: string) => /^[a-z]$/.test(s);
